a49dcab2d1850623bc2908fd16730ee7cb31376d
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-04-29 09:35:19 -0400

    updated ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import "../styles/global.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "degen";
+import "degen/styles";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
